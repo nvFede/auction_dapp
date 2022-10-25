709b4e823d2c4d8f8fb23024ee174b72ec2ee2ae
Federico <federico@MacBook-Pro-de-Federico-2.local>
2022-10-25 11:21:38 +0200

    first commit

---
+++ webapp/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  
+  <Navbar-Item></Navbar-Item>
+</template>
+
+<script setup>
+
+import NavbarItem from './components/Navbar-Item.vue'
+
+
+  name: 'App'
+  const components = () => {
+    NavbarItem
+  }
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
